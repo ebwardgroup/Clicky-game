edb411eb8615ecfd1fca1f229a68cfd99f2d8d22
Eric Ward <ebwardgroup@gmail.com>
2020-05-31 18:30:16 -0600

    pushing final version

---
+++ my-app/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Clicky Game!</h1>
+      <h2>Click on each character only once in order to earn points.  It's harder than it sounds because images will shuffle after every click.  To restart, click on any image again.   Enjoy exercising your memory!</h2>
+    </header>
+  );
+}
+
+
+export default Header;
